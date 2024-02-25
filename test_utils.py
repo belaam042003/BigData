4bab78bd0da348b70ba1c193a6a66113dfcbd9a8
Belaam042003 <isabella.ardila02@usa.edu.co>
2024-02-24 20:33:36 -0500

    Codigo con pruebas unitarias y funciones de sumar y restar

---
+++ test_utils.py
@@ -0,0 +1,13 @@
+from utils import sumar, restar
+
+
+def test_sumar():
+    assert sumar(2, 4) == 6
+    assert sumar(1, 4) == 5
+    assert sumar(2, 2) == 4
+
+
+def test_restar():
+    assert restar(6, 4) == 2
+    assert restar(1, 4) == -3
+    assert restar(2, 2) == 0
